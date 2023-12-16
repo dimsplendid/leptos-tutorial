7131b6ee252e9f27f3f27ae63376b32503e02a1c
dimsplendid <dimsplendid@gmail.com>
2023-12-17 00:22:54 +0800

    Leptos tutorial setup

---
+++ README.md
@@ -0,0 +1,18 @@
+# README
+
+## Set up
+
+1. rust
+    ```bash
+    rustup toolchain install nightly
+    rustup default nightly
+    rustup target add wasm32-unknown-unknown
+
+    ```
+2. trunk
+    ```bash
+    cargo install trunk
+
+    ```
+
+> Progress: [A Basic Component](https://book.leptos.dev/view/01_basic_component.html)
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use leptos::*;
+
+fn main() {
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            set_count(3);
+        }>
+
+            "Click me!" {move || count.get()}
+        </button>
+    }
+}
